2a3ed6204967656c8be705400fa52e41850e161f
DEAkimov <deakimov@edu.hse.ru>
2018-12-04 00:53:09 +0300

    change relu actiovation to selu, add nonlinearity to projection

--- src/network.py
+++ src/network.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torch.nn.functional import selu
 
 
 class Net(nn.Module):
@@ -6,9 +7,9 @@ class Net(nn.Module):
         super(Net, self).__init__()
         num_actions = 5
         self.conv = nn.Sequential(
-            nn.Conv2d(4, 32, kernel_size=8, stride=4), nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2), nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3), nn.ReLU(),
+            nn.Conv2d(4, 32, kernel_size=8, stride=4), nn.SELU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2), nn.SELU(),
+            nn.Conv2d(64, 64, kernel_size=3), nn.SELU(),
         )
         self.projector = nn.Linear(1344, 512)
         self.policy_layer = nn.Linear(512, num_actions)
@@ -17,7 +18,7 @@ class Net(nn.Module):
     def forward(self, screen):
         batch = screen.size(0)
         conv_features = self.conv(screen)  # [batch, 64, 7, 3]
-        features = self.projector(conv_features.view(batch, -1))
+        features = selu(self.projector(conv_features.view(batch, -1)))
         policy = self.policy_layer(features)
         value = self.value_layer(features).squeeze(-1)
         return policy, value
